5adf60e5fe5acb30f7431570ae007b51fe72ad0a
Mikhail <89340815+Karabanoff87@users.noreply.github.com>
2023-05-05 13:22:03 +0300

    step5
    -added swiper for reviews section
    -bug fixes

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//Swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spacebetween: 10,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+
+  });
